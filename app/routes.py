6784a62ea63945e907a8d95ca64ca3c83bf08c59
EC2 Default User <ec2-user@ip-172-31-18-197.us-east-2.compute.internal>
2019-07-20 16:14:51 +0000

    added all the base files

---
+++ app/routes.py
@@ -0,0 +1,10 @@
+from app import app
+from flask import render_template, request
+from app.models import model, formopener
+import math
+import random
+
+@app.route('/')
+@app.route('/index')
+def index():
+    return render_template('index.html')
